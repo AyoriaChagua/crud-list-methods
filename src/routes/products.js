9521b83901912461fad9fc9cc779ce63e3d72fb7
unknown <ayoriachagua@gmail.com>
2023-05-10 19:55:56 -0500

    first commit lab08

---
+++ src/routes/products.js
@@ -0,0 +1,89 @@
+import Router from "express"
+
+export const router_products = Router()
+
+let products = [
+    {
+        id: 1,
+        name: "Laptop",
+        brand: "Apple",
+        price: 9999.99,
+        stock: 20
+    },
+    {
+        id: 2,
+        name: "iPhone",
+        brand: "Apple",
+        price: 3000.90,
+        stock: 25
+    },
+    {
+        id: 3,
+        name: "iPad",
+        brand: "Apple",
+        price: 5000.95,
+        stock: 23
+    },
+]
+
+router_products.get('/products', (req, res)=>{
+    res.render("products", {products})
+})
+
+router_products.post('/products', (req, res)=>{
+    const product = req.body
+    products.push(product)
+    res.redirect("/products")
+})
+
+
+
+router_products.get('/product/delete/:id', (req, res)=>{
+    const { id } = req.params
+    products.forEach((product, index) => {
+        if(product.id == id){
+            products.splice(index, 1)
+            return
+        }
+    })
+    res.redirect("/products")
+})
+
+router_products.get('/product/:id', (req, res)=>{
+    const { id } = req.params
+
+    const product = products.find((product) => product.id == id);
+
+    res.render('product-edit', {product})
+    
+})
+
+router_products.post('/product/:id', (req, res)=>{
+    const { id } = req.params
+    
+    console.log(id)
+
+    const {id_up, name, price, brand, stock} = req.body
+
+ 
+    
+    const newproduct = {
+        id : id_up,
+        name,
+        brand,
+        price,
+        stock
+    }
+
+    
+    console.log(newproduct)
+
+    products.forEach((product, index)=>{
+        if(product.id == id){
+            products[index] = newproduct
+            return
+        }
+    })
+    res.redirect('/products')
+})
+
